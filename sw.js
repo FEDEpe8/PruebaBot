b6a3efa0b5033cdd0c4770969bf0333b7999962f
FEDEpe8 <fhernansperoni@gmail.com>
2026-02-06 19:46:29 -0300

    Update CACHE_NAME to version v58

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'muni-chascomus-v57';
+const CACHE_NAME = 'muni-chascomus-v58';
 const assets = [ './', './index.html', './style.css', './script.js', './manifest.json', './logo.png' ];
 
 self.addEventListener('install', e => {
