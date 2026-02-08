d52678a13619b327fb363123c98ff7387e2a1685
FEDEpe8 <fhernansperoni@gmail.com>
2026-02-08 11:39:39 -0300

    Update cache name to 'muni-chascomus-v66'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'muni-chascomus-v65';
+const CACHE_NAME = 'muni-chascomus-v66';
 const assets = [ './', './index.html', './style.css', './script.js', './manifest.json', './logo.png' ];
 
 self.addEventListener('install', e => {
